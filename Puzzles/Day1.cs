3331adcbe2fdacfaac1bb2b3ead36a3558d95b35
Kevin Renato <kev.renato@gmail.com>
2023-12-01 21:02:55 -0500

    Day 1.

--- Puzzles/Day1.cs
+++ Puzzles/Day1.cs
@@ -2,15 +2,93 @@
 
 internal class Day1(string dataPath) : Puzzle(dataPath)
 {
+    private readonly Dictionary<string, string> _digits = new()
+    {
+        { "0", "zero" },
+        { "1", "one" },
+        { "2", "two" },
+        { "3", "three" },
+        { "4", "four" },
+        { "5", "five" },
+        { "6", "six" },
+        { "7", "seven" },
+        { "8", "eight" },
+        { "9", "nine" }
+    };
+
     public override async Task<string> GetPart1AnswerAsync()
     {
         var data = await GetDataAsync();
-        return data[0] + " Part 1";
+
+        int total = 0;
+
+        foreach (var value in data)
+        {
+            var firstDigit = value.First(char.IsDigit);
+            var lastDigit = value.Last(char.IsDigit);
+
+            var number = int.Parse(firstDigit.ToString() + lastDigit.ToString());
+
+            total += number;
+        }
+
+        return $"Calibration value: {total}";
     }
 
     public override async Task<string> GetPart2AnswerAsync()
     {
         var data = await GetDataAsync();
-        return data[0] + " Part 2";
+
+        int total = 0;
+
+        foreach (var value in data)
+        {
+            var firstDigit = GetFirstDigit(value) ?? throw new InvalidOperationException("No digit found.");
+            var lastDigit = GetLastDigit(value) ?? throw new InvalidOperationException("No digit found.");
+
+            var number = int.Parse(firstDigit + lastDigit);
+
+            total += number;
+        }
+
+        return $"Calibration value: {total}";
+    }
+
+    private string? GetFirstDigit(string value)
+    {
+        for (int i = 0; i < value.Length; i++)
+        {
+            var digit = GetDigitAtIndex(value, i);
+            if (digit is not null)
+                return digit;
+        }
+
+        return null;
+    }
+
+    private string? GetLastDigit(string value)
+    {
+        for (int i = value.Length - 1; i >= 0; i--)
+        {
+            var digit = GetDigitAtIndex(value, i);
+            if (digit is not null)
+                return digit;
+        }
+
+        return null;
+    }
+
+    private string? GetDigitAtIndex(string value, int idx)
+    {
+        foreach (var digit in _digits)
+        {
+            if (value[idx].ToString() == digit.Key ||
+                (value.Length - idx >= digit.Value.Length && value[idx..(idx + digit.Value.Length)] == digit.Value))
+            {
+                return digit.Key;
+            }
+        }
+
+        return null;
     }
 }
